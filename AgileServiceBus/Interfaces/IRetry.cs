59045abccaddc9c55ffbd61eaba2552501a4bc79
Luca Nicolini <mithosk@alice.it>
2018-11-18 17:35:46 +0100

    interface for retry

---
+++ AgileServiceBus/Interfaces/IRetry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AgileServiceBus.Interfaces
+{
+    public interface IRetry : IExcludeForRetry, IIncludeForRetry
+    {
+        bool IsForRetry<TException>(TException exception) where TException : Exception;
+        Task ExecuteAsync(Func<Task> toRetry, Func<Exception, ushort, ushort, Task> error);
+    }
+}
