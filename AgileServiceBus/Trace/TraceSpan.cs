4423347283b03a194e8a29cf0484bba5fd38ef69
Luca Nicolini <mithosk@alice.it>
2019-02-16 18:03:37 +0100

    span for tracing

---
+++ AgileServiceBus/Trace/TraceSpan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AgileServiceBus.Trace
+{
+    public class TraceSpan
+    {
+        public Guid Id { get; set; }
+        public Guid? ParentId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string DisplayName { get; set; }
+        public Dictionary<string, string> Attributes { get; set; }
+    }
+}
