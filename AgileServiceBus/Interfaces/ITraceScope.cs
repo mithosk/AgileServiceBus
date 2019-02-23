21c1a61a72480ba47da6f05b77828cb5bd5a9695
Luca Nicolini <mithosk@alice.it>
2019-02-23 16:01:44 +0100

    fixed interface

--- AgileServiceBus/Interfaces/ITraceScope.cs
+++ AgileServiceBus/Interfaces/ITraceScope.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AgileServiceBus.Interfaces
 {
     public interface ITraceScope : IDisposable
     {
+        Guid SpanId { get; }
+        Dictionary<string, string> Attributes { get; }
+
         ITraceScope CreateSubScope(string displayName);
     }
 }
