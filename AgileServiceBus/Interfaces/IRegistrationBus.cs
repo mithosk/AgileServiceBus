9c2042fcc24d637f03691b8165fe659dc80b5b18
Luca Nicolini <mithosk@alice.it>
2019-03-03 01:35:42 +0100

    tracer registration

--- AgileServiceBus/Interfaces/IRegistrationBus.cs
+++ AgileServiceBus/Interfaces/IRegistrationBus.cs
@@ -1,4 +1,5 @@
 ﻿using AgileServiceBus.Interfaces;
+using AgileServiceBus.Trace;
 using Autofac;
 using FluentValidation;
 using System;
@@ -12,6 +13,7 @@ namespace AgileSB.Interfaces
 
         IIncludeForRetry Subscribe<TSubscriber, TRequest>(AbstractValidator<TRequest> validator) where TSubscriber : IRequestSubscriber<TRequest> where TRequest : class;
         IExcludeForRetry Subscribe<TSubscriber, TMessage>(string topic, ushort prefetchCount, AbstractValidator<TMessage> validator, string retryCron, ushort? retryLimit) where TSubscriber : IPublishSubscriber<TMessage> where TMessage : class;
+        void RegisterTracer<TTracer>() where TTracer : Tracer;
         void RegistrationCompleted();
     }
 }
