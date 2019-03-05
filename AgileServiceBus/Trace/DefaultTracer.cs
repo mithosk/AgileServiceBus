944d3be0171b222ff58e3dd0b97f78f182d6a64e
Luca Nicolini <mithosk@alice.it>
2019-03-05 15:18:18 +0100

    default tracer

---
+++ AgileServiceBus/Trace/DefaultTracer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AgileServiceBus.Trace
+{
+    public class DefaultTracer : Tracer
+    {
+        public async override Task TraceAsync(TraceSpan span)
+        {
+            string deep = span.ParentId == null ? "Root" : "Child";
+            string time = ((int)((span.EndTime - span.StartTime).TotalMilliseconds + 0.5)).ToString();
+            string displayName = span.DisplayName;
+
+            await Console.Out.WriteLineAsync("Trace:    " + deep.PadLeft(5, ' ') + " [" + time.PadLeft(5, ' ') + "]  " + displayName);
+        }
+    }
+}
