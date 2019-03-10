24fea49d3e6e9704fce6fe8df2d305cfc0024737
Luca Nicolini <mithosk@alice.it>
2019-03-10 01:25:46 +0100

    changed tracespan data types

--- AgileServiceBus/Tracing/TraceSpan.cs
+++ AgileServiceBus/Tracing/TraceSpan.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace AgileServiceBus.Trace
+namespace AgileServiceBus.Tracing
 {
     public class TraceSpan
     {
-        public Guid Id { get; set; }
-        public Guid? ParentId { get; set; }
+        public string Id { get; set; }
+        public string ParentId { get; set; }
+        public string TraceId { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
         public string DisplayName { get; set; }
