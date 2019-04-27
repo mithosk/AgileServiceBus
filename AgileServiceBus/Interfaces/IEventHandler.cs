db51e703be934a61262c4da2de464e9edbc52a92
Luca Nicolini <mithosk@alice.it>
2019-04-28 00:10:42 +0200

    event handler interface

---
+++ AgileServiceBus/Interfaces/IEventHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace AgileServiceBus.Interfaces
+{
+    public interface IEventHandler<TEvent> where TEvent : class
+    {
+        IMicroserviceBus Bus { get; set; }
+        ITraceScope TraceScope { get; set; }
+
+        Task HandleAsync(TEvent message);
+    }
+}
