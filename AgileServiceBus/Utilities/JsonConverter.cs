6953e6ecb507407af33b7f6ef8a0ebcae1590977
Luca Nicolini <mithosk@alice.it>
2019-08-10 01:20:54 +0200

    json converter

---
+++ AgileServiceBus/Utilities/JsonConverter.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using System.Globalization;
+
+namespace AgileServiceBus.Utilities
+{
+    public class JsonConverter
+    {
+        public static JsonSerializerSettings Settings { get; private set; }
+
+        static JsonConverter()
+        {
+            Settings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Culture = CultureInfo.InvariantCulture,
+                DateFormatHandling = DateFormatHandling.IsoDateFormat,
+                NullValueHandling = NullValueHandling.Ignore
+            };
+
+            Settings.Converters.Add(new StringEnumConverter()
+            {
+                AllowIntegerValues = false,
+                NamingStrategy = new CamelCaseNamingStrategy()
+            });
+        }
+
+        public string Serialize<TMessage>(TMessage message) where TMessage : class
+        {
+            return JsonConvert.SerializeObject(message, Settings);
+        }
+
+        public TMessage Deserialize<TMessage>(string message) where TMessage : class
+        {
+            return JsonConvert.DeserializeObject<TMessage>(message, Settings);
+        }
+    }
+}
