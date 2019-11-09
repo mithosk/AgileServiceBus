b045eb9246c89802645f0f1d181ed70c72543d38
Luca Nicolini <mithosk@alice.it>
2019-11-10 00:39:52 +0100

    add fileds

--- AgileServiceBus/Logging/MessageDetail.cs
+++ AgileServiceBus/Logging/MessageDetail.cs
@@ -6,6 +6,7 @@ namespace AgileServiceBus.Logging
     public class MessageDetail
     {
         public string Id { get; set; }
+        public string CorrelationId { get; set; }
         public MessageType Type { get; set; }
         public string Directory { get; set; }
         public string Subdirectory { get; set; }
@@ -13,5 +14,6 @@ namespace AgileServiceBus.Logging
         public string Body { get; set; }
         public string AppId { get; set; }
         public Exception Exception { get; set; }
+        public bool ToRetry { get; set; }
     }
 }
