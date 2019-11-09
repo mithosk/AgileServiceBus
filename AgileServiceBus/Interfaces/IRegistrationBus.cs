693d1ab8417116980b8b562765231cb0e1671632
Luca Nicolini <mithosk@alice.it>
2019-11-09 18:52:27 +0100

    extended interface

--- AgileServiceBus/Interfaces/IRegistrationBus.cs
+++ AgileServiceBus/Interfaces/IRegistrationBus.cs
@@ -1,4 +1,5 @@
 ﻿using AgileServiceBus.Interfaces;
+using AgileServiceBus.Logging;
 using AgileServiceBus.Tracing;
 using Autofac;
 using FluentValidation;
@@ -13,6 +14,7 @@ namespace AgileSB.Interfaces
 
         IIncludeForRetry Subscribe<TSubscriber, TRequest>(AbstractValidator<TRequest> validator) where TSubscriber : IResponder<TRequest> where TRequest : class;
         IExcludeForRetry Subscribe<TSubscriber, TEvent>(string tag, AbstractValidator<TEvent> validator, string retryCron, ushort? retryLimit) where TSubscriber : IEventHandler<TEvent> where TEvent : class;
+        void RegisterLogger<TLogger>() where TLogger : Logger;
         void RegisterTracer<TTracer>() where TTracer : Tracer;
         void RegistrationCompleted();
     }
