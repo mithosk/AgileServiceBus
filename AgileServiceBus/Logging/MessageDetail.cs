8343cf711ccecbbcfaa162f26854b6e389b99506
Luca Nicolini <mithosk@alice.it>
2019-11-09 01:10:27 +0100

    class for message log

---
+++ AgileServiceBus/Logging/MessageDetail.cs
@@ -0,0 +1,17 @@
+﻿using AgileServiceBus.Enums;
+using System;
+
+namespace AgileServiceBus.Logging
+{
+    public class MessageDetail
+    {
+        public string Id { get; set; }
+        public MessageType Type { get; set; }
+        public string Directory { get; set; }
+        public string Subdirectory { get; set; }
+        public string Name { get; set; }
+        public string Body { get; set; }
+        public string AppId { get; set; }
+        public Exception Exception { get; set; }
+    }
+}
